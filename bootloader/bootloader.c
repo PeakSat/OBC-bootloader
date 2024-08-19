1ff13cf85fefc5f32be8a1720248b73e6ba169cd
Ilia Zarka <iliazark@gamil.com>
2024-08-19 17:27:40 +0300

    cleanup

--- bootloader/bootloader.c
+++ bootloader/bootloader.c
@@ -14,35 +14,34 @@ __attribute__((naked)) static void start_app(uint32_t pc, uint32_t sp)  {
 
 int bootloader(void);
 
-
 typedef struct _v {
   void* stack_pointer;
   void* pc;
 } Vectors;
 
-__attribute__((section(".vectors")))
+__attribute__((section(".vectors"))) // start binary with the stack pointer and program counter
 const Vectors myVec = {
   .stack_pointer = (void*) (&__ram_start__), // "&" does not dereference a pointer. The value of __ram_start__is put to the .stack_pointer
   .pc = (void*) &bootloader
 };
 
 int bootloader(void) {
-    //dereference stack pointer and reset handler pointer
-    // __asm("nop");
 
-
+    //store variabes in a buffer
     uint32_t *data = (uint32_t *) &__variables_start__;// "&" does not dereference a pointer. The value of __variables_start__is put to the data pointer
     uint32_t data_buffer[128];
     for(uint32_t i=0; i<128; i++)
     {
       data_buffer[i] = data[i];
     }
-    uint32_t bootrom_choise=0;
+
+
     /**
     * data_buffer[0] hold the number of unsuccesful boot tries from the primary partition
     * data_buffer[1] holds the ID of the primary boot partition (either 0 or 1)
     */
 
+    uint32_t bootrom_choise=0;
     if(data_buffer[0]<3) //this variable should be set to 0 on succesful boot
     {
       data_buffer[0]++;
@@ -66,7 +65,7 @@ int bootloader(void) {
 
   //erase variables
 
-  uint32_t variables= (uint32_t)(&__variables_start__);
+  uint32_t variables= (uint32_t)(&__variables_start__);// "&" does not dereference a pointer. The value of __variables_start__is put to the data pointer
   uint32_t iflash_page_size=512;
 
   uint32_t *eefc_fcr = (uint32_t*)(0x400e0c00 + 0x04);
@@ -88,19 +87,13 @@ int bootloader(void) {
   {
     *(((uint32_t *)( variables)+(i/4U))) = data_buffer[i/4U];
     __asm("dmb 0xF":::"memory"); //wait for all previous explicit memory accesses to be observed
-    // __DMB();
   }
-
-/**< Offset: 0x04 ( /W  32) EEFC Flash Command Register */
 
   uint32_t write_pageCommand=0x00000001;
   page_number = ((variables - iflash_start) / iflash_page_size);
   arg = 0xFFFF & (page_number << 8);
 
   *eefc_fcr = write_pageCommand | arg | passwd;
-
-
-
 
   __asm volatile ("dsb 0xF":::"memory"); //wait for all previous instructions to be completed
   __asm volatile ("isb 0xF":::"memory"); //flush the pipeline
