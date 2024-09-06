d80a63a6211ea250abaa1441684b284136e35c63
Ilia Zarka <iliazark@gamil.com>
2024-09-06 15:24:25 +0300

    fixed CMake

---
+++ bootloader/include/bootloader.h
@@ -0,0 +1,20 @@
+//
+// Created by iliaz on 8/21/2024.
+//
+
+#ifndef BOOTLOADER_H
+#define BOOTLOADER_H
+
+#define IFLASH_PAGE_SIZE                    512                    /* size of the internal flash pages*/
+#define MAX_REBOOT_TRIES                    3                      /* number of unsuccesful reboots before switching to the secondary firmware*/
+#define FIRST_FIRMWARE_IS_PRIMARY           0                      /* the second variable in the variables partition should be 0 if the first firmware is the primary one*/
+#define SECOND_FIRMWARE_IS_PRIMARY          1                      /* the second variable in the variables partition should be 1 if the second firmware is the primary one*/
+#define EEFC_FCR_FCMD_WP                    0x00000001             /* command to write a page*/
+#define EEFC_FCR_FCMD_EP                    0x00000007             /* command to erase pages. see "Erase Commands" section for more information*/
+#define EEFC_FCR_PASSWD                     0x5A000000             /* password for the EEFC_FCR register*/
+#define EEFC_FCR                            0x400e0c04             /* EEFC + FCR offset (0x400e0c00 + 0x04)*/
+#define BOOT_COUNTER                        0                      /* Counter place in the variables partition*/
+#define PRIMARY_PARTITION_VARIABLE          1                      /* primary partition variable place in the variables partition*/
+
+
+#endif //BOOTLOADER_H
---
+++ bootloader/src/bootloader.c
@@ -0,0 +1,98 @@
+//
+// Created by iliaz on 6/8/2024.
+//
+#include <inttypes.h>
+#include "memory_map.h"
+#include "bootloader.h"
+
+__attribute__((naked)) static void start_app(uint32_t pc, uint32_t sp)  {
+  __asm("           \n\
+        msr msp, r1 /* load r1 into MSP */\n\
+        bx r0       /* branch to the address at r0 */\n\
+  ");
+}
+
+int bootloader(void);
+
+typedef struct _v {
+  void* stack_pointer;
+  void* pc;
+} Vectors;
+
+__attribute__((section(".vectors"))) // start binary with the stack pointer and program counter
+const Vectors myVec = {
+  .stack_pointer = (void*) (&__ram_start__), // "&" does not dereference a pointer. The value of __ram_start__is put to the .stack_pointer
+  .pc = (void*) &bootloader
+};
+
+int bootloader(void) {
+
+    //store variabes in a buffer
+    uint32_t *data = (uint32_t *) &__variables_start__;// "&" does not dereference a pointer. The value of __variables_start__is put to the data pointer
+    uint32_t data_buffer[IFLASH_PAGE_SIZE/sizeof(uint32_t)];
+    for(uint32_t i=0; i<IFLASH_PAGE_SIZE/sizeof(uint32_t); i++)
+    {
+      data_buffer[i] = data[i];
+    }
+
+    /**
+    * data_buffer[BOOT_COUNTER] hold the number of unsuccesful boot tries from the primary partition
+    * data_buffer[PRIMARY_PARTITION_VARIABLE] holds the ID of the primary boot partition (either 0 or 1)
+    */
+
+    if(data_buffer[BOOT_COUNTER]>MAX_REBOOT_TRIES){
+      data_buffer[BOOT_COUNTER]=0;
+      if(data_buffer[PRIMARY_PARTITION_VARIABLE]==FIRST_FIRMWARE_IS_PRIMARY){
+        data_buffer[PRIMARY_PARTITION_VARIABLE]=SECOND_FIRMWARE_IS_PRIMARY;
+      }else{
+        data_buffer[PRIMARY_PARTITION_VARIABLE]=FIRST_FIRMWARE_IS_PRIMARY;
+      }
+    }else{
+      data_buffer[BOOT_COUNTER]++;
+    }
+
+
+  /*erase variables partition*/
+  uint32_t variables= (uint32_t)(&__variables_start__);// "&" does not dereference a pointer. The value of __variables_start__is put to the data pointer
+  uint32_t boot_start= (uint32_t)(&__bootrom_start__);// "&" does not dereference a pointer. The value of __variables_start__is put to the data pointer
+  uint32_t *eefc_fcr = (uint32_t*)EEFC_FCR; //pointer to FCR register
+
+  uint32_t page_number = ((variables - boot_start) / IFLASH_PAGE_SIZE)|0x2U; //0x2U to clear 16 pages(the size of the variables partition)
+  uint32_t arg = 0xFFFF & (page_number << 8);
+
+  *eefc_fcr = EEFC_FCR_FCMD_EP | arg | EEFC_FCR_PASSWD; // [0:7] CMD, [8:23] ARGS, [24:31]PASSWORD
+
+  __asm("dmb 0xF":::"memory"); //wait for all previous explicit memory accesses to be observed
+  __asm volatile ("dsb 0xF":::"memory"); //wait for all previous instructions to be completed
+  __asm volatile ("isb 0xF":::"memory"); //flush the pipeline
+
+
+  /*write the variables to the flash*/
+  for (uint32_t i = 0; i < IFLASH_PAGE_SIZE; i += 4U)
+  {
+    *(((uint32_t *)( variables)+(i/4U))) = data_buffer[i/4U];
+    __asm("dmb 0xF":::"memory"); //wait for all previous explicit memory accesses to be observed
+  }
+
+  page_number = ((variables - boot_start) / IFLASH_PAGE_SIZE);
+  arg = 0xFFFF & (page_number << 8);
+
+  *eefc_fcr = EEFC_FCR_FCMD_WP | arg | EEFC_FCR_PASSWD; // [0:7] CMD, [8:23] ARGS, [24:31]PASSWORD
+
+  __asm volatile ("dsb 0xF":::"memory"); //wait for all previous instructions to be completed
+  __asm volatile ("isb 0xF":::"memory"); //flush the pipeline
+
+  /*branch to the correct firmware*/
+  uint32_t *app_code;
+  if(data_buffer[PRIMARY_PARTITION_VARIABLE]==SECOND_FIRMWARE_IS_PRIMARY){
+    app_code = (uint32_t *) &__approm2_start__;
+  }else{
+    app_code = (uint32_t *) &__approm1_start__;
+  }
+  uint32_t app_sp = app_code[BOOT_COUNTER];
+  uint32_t app_start = app_code[PRIMARY_PARTITION_VARIABLE];
+  start_app(app_start,app_sp);
+
+    /* Not Reached */
+    while (1) {}
+}
