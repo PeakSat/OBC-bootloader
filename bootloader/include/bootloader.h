9778ed9350500b259fb5760a1e604e6bded5956f
iliazark <56551628+iliazark@users.noreply.github.com>
2024-09-16 16:44:46 +0300

    updated comments

--- bootloader/include/bootloader.h
+++ bootloader/include/bootloader.h
@@ -8,8 +8,8 @@
 #define IFLASH_PAGE_SIZE                    512                    /* size of the internal flash pages*/
 #define IFLASH_START                        0x00400000             /* start of the internal flash*/
 #define MAX_REBOOT_TRIES                    3                      /* number of unsuccesful reboots before switching to the secondary firmware*/
-#define FIRST_FIRMWARE_ID           0                      /* the second variable in the variables partition should be 0 if the first firmware is the primary one*/
-#define SECOND_FIRMWARE_ID          1                      /* the second variable in the variables partition should be 1 if the second firmware is the primary one*/
+#define FIRST_FIRMWARE_ID                   0                      /* the second variable in the variables partition should be 0 if the first firmware is the primary one*/
+#define SECOND_FIRMWARE_ID                  1                      /* the second variable in the variables partition should be 1 if the second firmware is the primary one*/
 #define EEFC_FCR_FCMD_WP                    0x00000001             /* command to write a page*/
 #define EEFC_FCR_FCMD_EP                    0x00000007             /* command to erase pages. see "Erase Commands" section for more information*/
 #define EEFC_FCR_FCMD_SLB                   0x00000008             /* command to lock a region*/
@@ -19,7 +19,7 @@
 #define EEFC_FCR                            0x400e0c04             /* EEFC + FCR offset (0x400e0c00 + 0x04)*/
 #define BOOT_COUNTER_VARIABLE               0                      /* Counter place in the variables partition*/
 #define PRIMARY_PARTITION_VARIABLE          1                      /* primary partition variable place in the variables partition*/
-#define BOOT_STATUS_VARIABLE                2    /* current boot status variable in the variables partition*/
+#define BOOT_STATUS_VARIABLE                2                      /* current boot status variable in the variables partition*/
 
 
 #endif //BOOTLOADER_H
